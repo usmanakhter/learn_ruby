3224a7860cb4fce8e7cdc5bf819501fd5202a75f
Usman Akhter <usmanakhter1@gmail.com>
2017-12-27 23:41:54 -0600

    progress on 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,16 @@
-#write your code here
+def echo(str)
+	str
+end
+
+def shout(str)
+	str.upcase
+end
+
+def repeat (input, n=nil)
+	if n == nil
+		input + " " + input
+	else
+		string = (input + " ") * n
+		return string.strip
+	end
+end
